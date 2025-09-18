ffc99d20b58a0bb2cfb3d953a9503abf434b217a
Sidharth Suresh <sidharthsuresh.work@gmail.com>
2025-09-18 16:59:52 +0530

    Update hello.c with new pointer example

--- hello.c
+++ hello.c
@@ -1,25 +1,41 @@
+// #include <stdio.h>
+
+// int main() {
+//     // 1. A normal integer variable (the "house")
+//     int num = 42;
+
+//     // 2. A pointer variable that can store the address of an integer.
+//     // The asterisk here declares it as a pointer.
+//     int *p;
+
+//     // 3. Let's store the address of 'num' in our pointer.
+//     // Read this as: "The pointer 'ptr' gets the address of 'num'".
+//     p = &num;
+
+//     // Now let's see what we have!
+//     printf("--- Let's inspect our variables ---\n");
+//     printf("The value of the variable 'num' is: %d\n", num);
+//     printf("The memory address of 'num' is: %p\n", &num); // Use %p for printing addresses
+
+//     printf("\n--- Let's inspect our pointer ---\n");
+//     printf("The value of the pointer 'ptr' is: %p\n", p); // Note: this is the same address as above!
+//     printf("The value AT THE ADDRESS the pointer is pointing to is: %d\n", *p); // We use '*' to get the value
+
+//     return 0;
+// }
+
+
+
+
+
 #include <stdio.h>
-
-int main() {
-    // 1. A normal integer variable (the "house")
-    int num = 42;
-
-    // 2. A pointer variable that can store the address of an integer.
-    // The asterisk here declares it as a pointer.
-    int *ptr;
-
-    // 3. Let's store the address of 'num' in our pointer.
-    // Read this as: "The pointer 'ptr' gets the address of 'num'".
-    ptr = &num;
-
-    // Now let's see what we have!
-    printf("--- Let's inspect our variables ---\n");
-    printf("The value of the variable 'num' is: %d\n", num);
-    printf("The memory address of 'num' is: %p\n", &num); // Use %p for printing addresses
-
-    printf("\n--- Let's inspect our pointer ---\n");
-    printf("The value of the pointer 'ptr' is: %p\n", ptr); // Note: this is the same address as above!
-    printf("The value AT THE ADDRESS the pointer is pointing to is: %d\n", *ptr); // We use '*' to get the value
-
+int main(){
+    int n=16;
+    int *p;
+    p=&n;
+    printf("value of n is %d\n",n);
+    printf("address of n is %p\n",&n);
+    printf("value of p is %p\n",p);
+    printf("value at address p is %d\n",*p);
     return 0;
 }
