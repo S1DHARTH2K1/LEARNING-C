d71be1488d52fa137402d7bfd1eeecc497d5182d
Sidharth Suresh <sidharthsuresh.work@gmail.com>
2025-09-18 18:29:35 +0530

    Update hello.c with latest code changes

--- hello.c
+++ hello.c
@@ -1,3 +1,4 @@
+//PROGRAM 1
 // #include <stdio.h>
 
 // int main() {
@@ -23,19 +24,39 @@
 
 //     return 0;
 // }
+// ------------------------------------------------------------
 
 
 
+//PROGRAM 2
+// #include <stdio.h>
+// int main(){
+//     int n=16;
+//     int *p;
+//     p=&n;
+//     printf("value of n is %d\n",n);
+//     printf("address of n is %p\n",&n);
+//     printf("value of p is %p\n",p);
+//     printf("value at address p is %d\n",*p);
+//     return 0;
+// }
+//------------------------------------------------------------
 
 
-#include <stdio.h>
-int main(){
-    int n=16;
-    int *p;
-    p=&n;
-    printf("value of n is %d\n",n);
-    printf("address of n is %p\n",&n);
-    printf("value of p is %p\n",p);
-    printf("value at address p is %d\n",*p);
-    return 0;
-}
+
+// TASK 1 - PROGRAM 3
+// Modify the program above:
+// 1. Create a new variable of type float called price and give it a value (e.g., 99.95).
+// 2. Create a pointer that can hold the address of a float.
+// 3. Make that pointer point to price.
+// 4. Print the value and address of price, and the value and pointed-to value of your new pointer.
+
+// #include <stdio.h>
+// int main(){
+//     float price=99.95;
+//     float *ptr;
+//     ptr=&price;
+//     printf("value of price is %.2f\n",price);
+//     printf("address of price is %p\n",&price);
+// }
+//------------------------------------------------------------
