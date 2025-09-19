8d2c738bdd05fbea455357e674b2661cbe9bb0d4
Sidharth Suresh <sidharthsuresh.work@gmail.com>
2025-09-19 17:56:38 +0530

    update chapter2.c

--- chapter2.c
+++ chapter2.c
@@ -35,3 +35,26 @@
 //     return 0;
 // }
 // ------------------------------------------------------------
+
+
+
+//TASK 1 - PROGRAM 3
+// Write a function called halve() that takes a pointer to a float as an argument.
+// The function should divide the value of the variable it's pointing to by two.
+// Test it in main with your price variable from the last exercise.
+
+// #include <stdio.h>
+// void halve(float *p) {
+//     *p = *p / 2.0f;
+// }
+// int main() {
+//     float price = 99.95f;
+//     printf("1. The original price is: %f\n", price);
+//     printf("2. Calling the 'halve' function and giving it the address of 'price'...\n");
+//     halve(&price);
+//     printf("3. Back in main, the new price is: %f\n", price);
+//     halve(&price);
+//     printf("4. Again back in main, the new price is: %f\n", price);
+//     return 0;
+// }
+// ------------------------------------------------------------
